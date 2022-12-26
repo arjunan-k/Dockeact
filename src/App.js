994e0fe6ffe7ab09cf42f7cac6d4b7a226b88c5f
Arjunan K <arjunank680620@gmail.com>
2022-12-26 15:07:52 +0530

    updated the intro

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Arjunan Bye!
+          Hello Arjunan keep going!
         </p>
         <a
           className="App-link"
